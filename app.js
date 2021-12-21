8ba637682b530f100405c1e7ba123c335cf8915f
matthew chimenti <matthewosu@gmail.com>
2021-12-20 17:46:23 -0800

    resturing into a basic javascript file. Battle counter works

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-
-
-let cat = 'cat'
-
-
-
 const App = {
     data() {
         return{ 
@@ -14,7 +8,6 @@ const App = {
             }
     },
     methods: {
-        
         makeGrid() {
             const grid = document.getElementById('grid')
             let counter = 0
@@ -27,59 +20,73 @@ const App = {
                     grid.innerHTML += '<div class="'+counter+' squares"></div>'
                     counter += 1
                 }
-            
             }
         },
-        testFunc() {
-            this.playerId = this.player.className
-            this.playerId = parseInt(this.playerId)
-            squares[this.playerId].removeAttribute('id')
-            squares[this.playerId].classList.add('squares')
-            squares[this.playerId].style.backgroundImage = "url('images/grass.png')"
-            squares[this.playerId + 1].id = 'player'
-            squares[this.playerId + 1].classList.remove('squares')
-            squares[this.playerId + 1].style.backgroundImage = "url('images/redRight.png'), url('images/grass.png')"
-        }
-
-
-
-
+        
+        randomEncounter() {
+            console.log('battle')
+        },
     },
+    
     mounted() {
         fetch('https://pokeapi.co/api/v2/pokemon/charmander')
         .then(response => response.json())
         .then(data => {
+
             this.charmander = data.sprites.front_default
-            console.log('test')
+            console.log(this.charmander)
         })
         this.makeGrid()
         
-
+        document.addEventListener("keydown", function (event) {
+            if (event.key == 'Enter'){
+                document.getElementById('introScreen').style.display = 'none'
+            }
+        })
  
         let squares = Array.from(document.querySelectorAll('#grid div'))
 
+        let battleCounter = 0
+        let battleTrigger = 5
         // Player Moves Right
         document.addEventListener("keydown", function (event) {
+
+            function encounter() {
+                battleCounter += 1
+                console.log(battleCounter)
+                if (battleCounter == battleTrigger) {
+                   alert('battle')
+                   battleCounter = -1
+                   battleTrigger = Math.floor(Math.random() * 5)
+                }
+            }
+
+
             if (event.key == 'ArrowRight'){
                 this.player = document.getElementById('player')
 
                 // Stop player from moving outside boundary
                 if (this.player.classList[0] % 10 == 9){
-
                 }
                 else{
-                    this.testFunc()
+                    this.playerId = this.player.className
+                    this.playerId = parseInt(this.playerId)
+                    squares[this.playerId].removeAttribute('id')
+                    squares[this.playerId].classList.add('squares')
+                    squares[this.playerId].style.backgroundImage = "url('images/grass.png')"
+                    squares[this.playerId + 1].id = 'player'
+                    squares[this.playerId + 1].classList.remove('squares')
+                    squares[this.playerId + 1].style.backgroundImage = "url('images/redRight.png'), url('images/grass.png')"
+                    encounter()
                     }
                 }
-            })
-        // Player Moves Left
-        document.addEventListener("keydown", function (event) {
-            if (event.key == 'ArrowLeft'){
+      
+            // Player Moves Left
+            else if (event.key == 'ArrowLeft'){
                 this.player = document.getElementById('player')
                 
                 // Stop player from moving outside boundary
                 if (this.player.classList[0] % 10 == 0){
-
                 }
 
                 else{ 
@@ -91,19 +98,16 @@ const App = {
                     squares[this.playerId - 1].id = 'player'
                     squares[this.playerId - 1].classList.remove('squares')
                     squares[this.playerId - 1].style.backgroundImage = "url('images/redLeft.png'), url('images/grass.png')"
+                    encounter()
                     }
                 }
-            })
 
-
-        // Player Moves Down
-        document.addEventListener("keydown", function (event) {
-            if (event.key == 'ArrowDown'){
+            // Player Moves Down
+            else if (event.key == 'ArrowDown'){
                 this.player = document.getElementById('player')
 
                 // Stop player from moving outside boundary
                 if (this.player.classList[0] > 89){
-
                 }
                 else {
                     this.playerId = this.player.className
@@ -114,19 +118,16 @@ const App = {
                     squares[this.playerId + 10].id = 'player'
                     squares[this.playerId + 10].classList.remove('squares')
                     squares[this.playerId + 10].style.backgroundImage = "url('images/redFront.png'), url('images/grass.png')"
-
+                    encounter()
                     }
                 }
-            })
-
-        // Player Moves Up
-        document.addEventListener("keydown", function (event) {
-            if (event.key == 'ArrowUp'){
+          
+            // Player Moves Up
+            else if (event.key == 'ArrowUp'){
                 this.player = document.getElementById('player')
 
                 // Stop player from moving outside boundary
                 if (this.player.classList[0] < 10){
-
                 }
                 else{
                     this.playerId = this.player.className
@@ -137,9 +138,11 @@ const App = {
                     squares[this.playerId - 10].id = 'player'
                     squares[this.playerId - 10].classList.remove('squares')
                     squares[this.playerId - 10].style.backgroundImage = "url('images/redBack.png'), url('images/grass.png')"
+                    encounter()
                     }
                 }
             })
+
     }
 
   }
--- app2.js
+++ app2.js
@@ -1,22 +1,128 @@
-// const Counter2 = {
-//     data() {
-//       return {
-//         counter2: 0,
-//         charmander: ''
-//       }
-//     },
-//     mounted() {
-//         fetch('https://pokeapi.co/api/v2/pokemon/charmander')
-//           .then(response => response.json())
-//           .then(data => {
-//               this.charmander = data.sprites.front_default
-//           })
+
+const grid = document.getElementById('grid')
+let counter = 0
+
+let player = ''
+let playerId = ''
+
+for (let i = 0; i < 100; i++){
+    if (i == 50){
+        grid.innerHTML += '<div id="player" class='+counter+'></div>'
+        counter += 1
+    }
+    else{
+        grid.innerHTML += '<div class="'+counter+' squares"></div>'
+        counter += 1
+    }
+}
+
+function randomEncounter() {
+    console.log('battle')
+}
 
 
-//       setInterval(() => {
-//         this.counter2++
-//       }, 1000)
-//     }
-//   }
-  
-//   Vue.createApp(Counter2).mount('#counter2')
+document.addEventListener("keydown", function (event) {
+    if (event.key == 'Enter'){
+        document.getElementById('introScreen').style.display = 'none'
+    }
+})
+
+let squares = Array.from(document.querySelectorAll('#grid div'))
+
+let battleCounter = 0
+let battleTrigger = 5
+
+// Player Moves Right
+document.addEventListener("keydown", function (event) {
+
+    function encounter() {
+        battleCounter += 1
+        console.log(battleCounter)
+        if (battleCounter == battleTrigger) {
+            alert('battle')
+            battleCounter = -1
+            battleTrigger = Math.floor(Math.random() * 5)
+        }
+    }
+
+
+    if (event.key == 'ArrowRight'){
+        player = document.getElementById('player')
+
+        // Stop player from moving outside boundary
+        if (player.classList[0] % 10 == 9){
+        }
+        else{
+            playerId = player.className
+            playerId = parseInt(playerId)
+            squares[playerId].removeAttribute('id')
+            squares[playerId].classList.add('squares')
+            squares[playerId].style.backgroundImage = "url('images/grass.png')"
+            squares[playerId + 1].id = 'player'
+            squares[playerId + 1].classList.remove('squares')
+            squares[playerId + 1].style.backgroundImage = "url('images/redRight.png'), url('images/grass.png')"
+            encounter()
+            }
+        }
+
+    // Player Moves Left
+    else if (event.key == 'ArrowLeft'){
+        player = document.getElementById('player')
+        
+        // Stop player from moving outside boundary
+        if (player.classList[0] % 10 == 0){
+        }
+
+        else{ 
+            playerId = player.className
+            playerId = parseInt(playerId)
+            squares[playerId].removeAttribute('id')
+            squares[playerId].classList.add('squares')
+            squares[playerId].style.backgroundImage = "url('images/grass.png')"
+            squares[playerId - 1].id = 'player'
+            squares[playerId - 1].classList.remove('squares')
+            squares[playerId - 1].style.backgroundImage = "url('images/redLeft.png'), url('images/grass.png')"
+            encounter()
+            }
+        }
+
+    // Player Moves Down
+    else if (event.key == 'ArrowDown'){
+        player = document.getElementById('player')
+
+        // Stop player from moving outside boundary
+        if (player.classList[0] > 89){
+        }
+        else {
+            playerId = player.className
+            playerId = parseInt(playerId)
+            squares[playerId].removeAttribute('id')
+            squares[playerId].classList.add('squares')
+            squares[playerId].style.backgroundImage = "url('images/grass.png')"
+            squares[playerId + 10].id = 'player'
+            squares[playerId + 10].classList.remove('squares')
+            squares[playerId + 10].style.backgroundImage = "url('images/redFront.png'), url('images/grass.png')"
+            encounter()
+            }
+        }
+    
+    // Player Moves Up
+    else if (event.key == 'ArrowUp'){
+        player = document.getElementById('player')
+
+        // Stop player from moving outside boundary
+        if (player.classList[0] < 10){
+        }
+        else{
+            playerId = player.className
+            playerId = parseInt(playerId)
+            squares[playerId].removeAttribute('id')
+            squares[playerId].classList.add('squares')
+            squares[playerId].style.backgroundImage = "url('images/grass.png')"
+            squares[playerId - 10].id = 'player'
+            squares[playerId - 10].classList.remove('squares')
+            squares[playerId - 10].style.backgroundImage = "url('images/redBack.png'), url('images/grass.png')"
+            encounter()
+            }
+        }
+    })
